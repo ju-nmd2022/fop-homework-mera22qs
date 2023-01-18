53482d6dbc04ba57a6ce581f6e63027726a2b0d5
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-01-18 11:53:52 +0100

    Emoji

---
+++ .github/emoji.js
@@ -0,0 +1,34 @@
+background(255, 255, 255);
+
+/*Head of the emoji*/
+fill(0, 200, 120);
+stroke(0, 0, 0);
+strokeWeight(2);
+ellipse(300, 200, 200, 200);
+
+/*Eyeballs of emoji*/
+fill(255, 255, 255);
+stroke(0, 0, 0);
+strokeWeight(2);
+ellipse(240, 180, 50, 50);
+ellipse(340, 180, 50, 50);
+
+/*black pupils of emoji */
+fill(0, 0, 0);
+stroke(0, 0, 0);
+strokeWeight(10);
+ellipse(231, 180, 20, 20);
+ellipse(331, 180, 20, 20);
+
+/*white pupil highlight emoji*/
+fill(255, 255, 255);
+stroke(255, 255, 255);
+strokeWeight(10);
+ellipse(225, 175, 5, 5);
+ellipse(325, 175, 5, 5);
+
+/*mouth of emoji*/
+fill(255, 255, 255);
+stroke(0, 0, 0);
+strokeWeight(2);
+ellipse(290, 250, 50, 90);
